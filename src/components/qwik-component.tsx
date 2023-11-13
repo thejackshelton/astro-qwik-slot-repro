85d416a86928460535d20e3fd708fc3556ac8387
thejackshelton <me@jackshelton.com>
2023-11-12 22:46:52 -0600

    first commit

---
+++ src/components/qwik-component.tsx
@@ -0,0 +1,9 @@
+// import { Slot, component$ } from "@builder.io/qwik";
+
+// export const MyQwikComponent = component$((props) => {
+//   return (
+//     <div {...props}>
+//       <Slot />
+//     </div>
+//   );
+// });
---
+++ src/components/react-component.tsx
@@ -0,0 +1,4 @@
+/** @jsxImportSource react */
+export const MyReactComponent = ({ children }: any) => {
+  return <div>{children}</div>;
+};
